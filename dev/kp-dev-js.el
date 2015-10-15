4b609c547d9ce2bfeec37c016472b951a9d4f67e
Kyle Purdon <kp@bit.ly>
2015-10-15 10:41:45 -0600

    Change js spacing

--- dev/kp-dev-js.el
+++ dev/kp-dev-js.el
@@ -5,7 +5,7 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 (setq js2-highlight-level 3)
-(setq-default js2-basic-offset 2)
+(setq-default js2-basic-offset 4)
 
 ;; (require 'flycheck)
 ;; (add-hook 'js-mode-hook
