68e21cb577ab99b20e93945849d0524a0bd56e9e
Kyle Purdon <kp@bit.ly>
2015-10-15 10:42:10 -0600

    Wider name column in ibuffer

--- ui/kp-ui.el
+++ ui/kp-ui.el
@@ -13,6 +13,14 @@
 
 (require 'neotree)
 
+;; expand the name column on ibuffer windows
+(setq ibuffer-formats
+      '((mark modified read-only " "
+              (name 50 50 :left :elide) " "
+              (size 9 -1 :right) " "
+              (mode 16 16 :left :elide) " " filename-and-process)
+        (mark " " (name 16 -1) " " filename)))
+
 (provide 'kp-ui)
 
 ;;; kp-ui.el ends here
